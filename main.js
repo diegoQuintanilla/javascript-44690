2d4612fe8c2428c5019365cf3a7f4b4276a7514b
DiegoQuintanilla <diego-q05@hotmail.com>
2023-01-17 07:28:03 -0300

    creacion de constructor persona y array de asistencia

--- main.js
+++ main.js
@@ -1,46 +1,70 @@
 
-//simulador de presentismo  
+//simulador de autentificacion  
 
-//creamos 3 empleados a manera de prueba con sus correspondientes ID
-const empleado1 = 5589;  //Diego
-const empleado2 = 4938;  //Hernan
-const empleado3 = 6789;  //Samuel
+//creamos el constructor de objetos 
+class Empleado {
+    constructor (nombre, apellido, ID, rango){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ID = ID;
+        this.rango = rango;
+    }
+}
 
+//creamos tres empleados a modo de prueba con sus correspondientes ID
+const empleado1 = new Empleado ("Diego","Ramirez",4958, "Supervisor");
+const empleado2 = new Empleado ("Hernan","Ramos",3381, "Encargado");
+const empleado3 = new Empleado ("Claudio","Gomez",1980, "Operador");
+
+
+const diaDeLaSemana = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
+let diaActual = new Date();
 let contador = 0;
-let ID;
+let numID;
+let asistencia = [];
 
-const saludar = nombre => alert ("***Bienvenido " + nombre + "***");
+
+//creamos la funcion flecha para automatizar el saludo de acceso 
+const saludar = nombre => alert ("***Bienvenido " + nombre + "***");   
+
+function asis() {
+    asistencia.push(diaDeLaSemana[diaActual.getDay()]);
+    asistencia.push(diaActual.getDate());
+}
+
 
 
 while (contador < 3) {
-    ID = Number(prompt("Por favor, ingrese su numero ID"));
+    numID = Number(prompt("Por favor, ingrese su numero ID"));
     
-    if (ID != ""){    // validacion de la variable ID 
+    if (numID != ""){    // validacion de la variable ID 
         
-        switch (ID) {
-            case empleado1: 
-                saludar("Diego")
-                contador=3
+        switch (numID) {
+            case empleado1.ID: 
+                saludar(empleado1.nombre);
+                asis();
+                contador=3;
                 break;
-            case empleado2:
-                saludar("Hernan")
-                contador=3
+            case empleado2.ID:
+                saludar(empleado2.nombre);
+                asis();
+                contador=3;
                 break;
-            case empleado3:
-                saludar("Samuel")
-                contador=3
+            case empleado3.ID:
+                saludar(empleado3.nombre);
+                asis();
+                contador=3;
                 break;
             default:
                 alert("su numero ID no corresponde a un empleado activo");
-        }
+        }        
 
     } else {
         alert ("No ingreso ningun numero ID");
-    }
+    }    
 
     contador++;
-}
-
+}    
 
 
 
