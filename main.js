01b1c2f0623695575cd176db66d11ce809bdafb7
DiegoQuintanilla <diego-q05@hotmail.com>
2023-02-07 01:33:47 -0300

    se le agrego un temporizador

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 let digito = "";
-
+let fechaActual = new Date();
+const diasDeLaSemana = [`domingo`,`lunes`,`martes`,`miercoles`,`jueves`,`viernes`,`sabado`];
+const mesDelAnio = [`enero`,`febrero`,`marzo`,`abril`,`mayo`,`junio`,`julio`,`agosto`,`septiembre`,`octubre`,`noviembre`,`diciembre`];
 
 //creamos el constructor de objetos 
 class Empleado {
@@ -10,14 +12,22 @@ class Empleado {
         this.ID = ID;
         this.rango = rango;
     }
-
+    
 }
 
 const numeros = document.querySelectorAll(".numeros");
 const cancelar = document.getElementById("cancelar");
 const aceptar = document.getElementById("aceptar");
 const lector_panel = document.getElementById("lector_panel");
+const hora = document.getElementById("hora");
+const fecha = document.getElementById("fecha");
 
+//CREACION DEL TEMPORIZADOR
+fecha.innerHTML = `${diasDeLaSemana[fechaActual.getDay()]} ${fechaActual.getDate()} de ${mesDelAnio[fechaActual.getMonth()]} del ${fechaActual.getFullYear()}`;
+setInterval(()=>{
+    let horaActual = new Date;
+    hora.innerHTML = horaActual.toLocaleTimeString();
+},1000)
 
 //creamos tres empleados a modo de prueba con sus correspondientes ID
 
@@ -29,7 +39,7 @@ const empleado1 = JSON.parse(localStorage.getItem("empleado 1"));
 const empleado2 = JSON.parse(localStorage.getItem("empleado 2"));
 const empleado3 = JSON.parse(localStorage.getItem("empleado 3"));
 
-
+lector_panel.innerText =`**Ingrese su numero ID **`;
 
 for (let i = 0; i < numeros.length; i++) {
     numeros[i].addEventListener("click", (e) => {
@@ -49,7 +59,7 @@ for (let i = 0; i < numeros.length; i++) {
 
 cancelar.addEventListener("click",cancelacion);
 function cancelacion(){
-    lector_panel.innerHTML = "";
+    lector_panel.innerText =`**Ingrese su numero ID **`;
     digito = "";
 }
 
@@ -68,22 +78,17 @@ aceptar.addEventListener("click",() => {
                 saludar(empleado3.nombre); 
                 break; 
             default:
-                lector_panel.innerText = `su numero ID es invalido`;
+                lector_panel.innerText = `** Su numero ID es invalido **`;
                 break;
         }
 
     } else {
-        lector_panel.innerText = "no ingreso ningun numero ID";
+        lector_panel.innerText = "** NO ingreso numero ID **";
     }
 
 } )
  
-const saludar = nombre => {lector_panel.innerText = `*** Bienvenido ${nombre} ***`};   
-
-
-
-
-
+const saludar = nombre => {lector_panel.innerText = `*** Bienvenido ${nombre} ***`};
 
 
 
